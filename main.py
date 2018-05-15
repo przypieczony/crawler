66123ddf1174681781ecc99d93e44f12a4a3bc86
kszymans <kamil.szymanski@nokia.com>
2018-05-15 13:31:08 +0200

    Add first prototype and update README file

---
+++ main.py
@@ -0,0 +1,19 @@
+# This is a prototype of our first app
+# Treat comments like pseduocode (https://en.wikipedia.org/wiki/Pseudocode)
+
+# We will be using 2 third-party libs: 
+# - BeautifulSoup (bs4)
+# - requests
+
+# Yes, all code will be written in English ;)
+
+
+# Regular expression (regexp in short) which finds web addresses 
+# through out page. It may be usefull in future crawling.
+
+# Function which gets url link as a arg and reurns data page.
+# This function must be well protected since it will be used many times.
+
+# Class which will find all links on page, returns only not yet visited
+# and differentiate external links from internal ones
+
