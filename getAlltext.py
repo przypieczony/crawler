5a9e54884e069cae55aa9eda23a429a0593376eb
Rafal <rafalg@hotmail.nl>
2018-05-18 21:13:54 +0200

    Poprawki

--- getAlltext.py
+++ getAlltext.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
-address = "https://help.github.com/articles/about-pull-requests/"
+address = "https://morefuzz.net/reviews/cegvera-creations-ep/"
 #html = requests.get(address)
 #soup = BeautifulSoup(html.content, "lxml")
 
@@ -12,7 +12,7 @@ def getAlltext(address):
     html = requests.get(address)
     soup = BeautifulSoup(html.content, "lxml")
     text_list = []
-    text_tags = ('p','span')
+    text_tags = ('p')
     for tag in text_tags:
         for text in soup.findAll(tag):
             text_list.append(text.get_text())
@@ -25,12 +25,42 @@ def splitToWord(text_list):
             words.append(word)
     return words
 
-def removePunctation(words_list):
-    p = re.compile("(\.|\!|\,|\?|\:|\;|\)|\(|\\|\/|\'|\")")
+#def removePunctation(words_list):
+#    p = re.compile("(\.|\!|\,|\?|\:|\;|\)|\(|\\|\/|\'|\")")
+#    new_word_list = []
+#    for word in words_list:
+#        word = p.sub(" ", word)        
+#        new_word_list.append(word)
+#    return new_word_list
+
+#def isWord(words_list):
+#    new_word_list = []
+#   numbers = ["0","1","2","3","4","5","6","7","8","9"]
+#    for word in words_list:
+#        word = word.lower()
+#        is_word = True
+#        for number in numbers:
+#            if number in word:
+#                is_word = False
+#                break
+#        if is_word == True:
+#            new_word_list.append(word)
+#    return new_word_list
+
+def standarizeWords(words_list):
     new_word_list = []
+    numbers = ["0","1","2","3","4","5","6","7","8","9"]
+    p = re.compile("(\.|\!|\,|\?|\:|\;|\)|\(|\\|\/|\'|\")")
     for word in words_list:
+        is_word = True
         word = p.sub(" ", word)
-        new_word_list.append(word)
+        word = word.lower()
+        for number in numbers:
+            if number in word:
+                is_word = False
+                break
+        if is_word == True:
+            new_word_list.append(word)
     return new_word_list
 
 def countWords(words_list):
@@ -53,6 +83,15 @@ def top5words(countWords_dict):
                 top_word = key
     return top_word
     
-    
-words = countWords(splitToWord(removePunctation(getAlltext(address))))
-TopWord = top5words(words)
+#def methode1(address):
+#    words = countWords(isWord(splitToWord(removePunctation(getAlltext(address)))))
+#    return words
+
+def methode2(address):
+    words = countWords(standarizeWords(splitToWord(getAlltext(address))))
+    return words
+
+#words1 = methode1(address)
+words2 = methode2(address)
+#TopWord1 = top5words(words1)
+TopWord2 = top5words(words2)
